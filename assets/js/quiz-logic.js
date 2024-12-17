c272123bf3375f2c34572cde035260145970c7b2
OmsUgan <Omer.Ugan@yh.nackademin.se>
2024-12-17 19:03:34 +0100

    show current quiz question

--- assets/js/quiz-logic.js
+++ assets/js/quiz-logic.js
@@ -2,9 +2,18 @@ import { QuizzesFromStorage } from "./classes.js";
 
 const quizzesFromStorage = QuizzesFromStorage();
 const quizAvailableDivBlock = document.getElementById("available-quizzes");
+const quizLayoutDivBlock = document.getElementById("quiz-layout");
+
+const quizQuestion = document.getElementById("quiz-question");
+const quizCounter = document.getElementById("quiz-question-counter");
+const quizOptions = document.getElementById("quiz-options");
+const quizProgressBar = document.getElementById("quiz-progressbar");
 
 let maxQuestions = 0;
 let questions = [];
+let currentQuestion = {};
+let currentQuestionIndex = 0;
+let correctAnswers = 0;
 
 export class Game {
 
@@ -18,6 +27,44 @@ export class Game {
     
         this.getQuestions(getSelectedQuiz);
         quizAvailableDivBlock.classList.add("hidden");
+        quizLayoutDivBlock.classList.remove("hidden");
+    }
+
+    getQuestions = (quiz) => {
+        currentQuestion = quiz.Questions[currentQuestionIndex];
+        console.log(currentQuestion);
+
+        quizQuestion.textContent = currentQuestion.QuestionText;
+        quizCounter.textContent = `Fråga ${currentQuestionIndex + 1} av ${maxQuestions}`;
+        quizProgressBar.style.width = `${((currentQuestionIndex + 1) / maxQuestions) * 100}%`;
+
+        quizOptions.innerHTML = "";
+
+        currentQuestion.Options.forEach((option, index) => {
+            const optionDiv = document.createElement("div");
+            optionDiv.classList.add("relative", "flex", "items-start");
+        
+            const optionInputDiv = document.createElement("div");
+            optionInputDiv.classList.add("flex", "h-10", "items-center");
+        
+            const optionInputRadio = document.createElement("input");
+            optionInputRadio.classList.add("relative", "size-4", "appearance-none", "rounded-full", "border", "border-gray-300", "bg-white", "before:absolute", "before:inset-1", "before:rounded-full", "before:bg-white", "checked:border-gray-400", "checked:bg-gray-400", "dark:checked:border-gray-800", "dark:checked:bg-gray-800", "focus-visible:outline", "focus-visible:outline-2", "focus-visible:outline-offset-2", "focus-visible:outline-indigo-600", "disabled:border-gray-300", "disabled:bg-gray-100", "disabled:before:bg-gray-400", "forced-colors:appearance-auto", "forced-colors:before:hidden", "[&:not(:checked)]:before:hidden");
+            optionInputRadio.type = "radio";
+            optionInputRadio.name = "answer";
+            optionInputRadio.value = option;
+            optionInputDiv.append(optionInputRadio);
+        
+            const optionLabelDiv = document.createElement("div");
+            optionLabelDiv.classList.add("ml-3", "text-sm/6");
+        
+            const optionLabel = document.createElement("label");
+            optionLabel.classList.add("text-2xl", "text-gray-900", "dark:text-white");
+            optionLabel.textContent = option;
+            
+            optionLabelDiv.append(optionLabel);
+            optionDiv.append(optionInputDiv, optionLabelDiv);
+            quizOptions.append(optionDiv);
+        });
     }
 
 }
